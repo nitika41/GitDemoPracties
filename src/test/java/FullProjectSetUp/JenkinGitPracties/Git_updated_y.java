22e7894e69837f0ab4e30099ada0ec708c73acc4
nitika41 <nitika.j@cisinlabs.com>
2020-03-09 20:19:40 +0530

    third commit

--- src/test/java/FullProjectSetUp/JenkinGitPracties/Git_updated_y.java
+++ src/test/java/FullProjectSetUp/JenkinGitPracties/Git_updated_y.java
@@ -1,5 +1,10 @@
 package FullProjectSetUp.JenkinGitPracties;
 
 public class Git_updated_y {
+	
+	public static void gitXUpdatedMethod() {
+		System.out.println("gitx uploaded code");
+		System.out.println("xyz");
+	}
 
 }

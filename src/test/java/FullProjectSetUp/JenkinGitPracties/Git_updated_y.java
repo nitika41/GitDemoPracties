4349dee7981a537f39f6e4b60a7bb45cd4750727
nitika41 <nitika.j@cisinlabs.com>
2020-03-13 14:14:08 +0530

    13 march commit

--- src/test/java/FullProjectSetUp/JenkinGitPracties/Git_updated_y.java
+++ src/test/java/FullProjectSetUp/JenkinGitPracties/Git_updated_y.java
@@ -5,6 +5,7 @@ public class Git_updated_y {
 	public static void gitXUpdatedMethod() {
 		System.out.println("gitx uploaded code");
 		System.out.println("xyz");
+		System.out.println("abc_xmaster");
 	}
 
 }
